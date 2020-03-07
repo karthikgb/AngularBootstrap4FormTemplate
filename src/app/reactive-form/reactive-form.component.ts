486972b52dae121a79c50145d7d90a2c9df79139
karthikgb <haikarthikgb@gmail.com>
2020-03-07 11:47:35 +0530

    upload image logic addedd12

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -9,7 +9,7 @@ import { CommonServiceService } from '../services/common-service.service';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor(private router: Router, public commonServiceService: CommonServiceService) { }
+  constructor(public router: Router, public commonServiceService: CommonServiceService) { }
 
 
   ngOnInit() {
